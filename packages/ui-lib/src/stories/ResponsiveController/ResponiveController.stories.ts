de75cfcb18db7443dd902c34c57f69c897367af6
codecraver12 <codekings98@gmail.com>
2024-05-21 13:33:53 +0100

    types add

--- packages/ui-lib/src/stories/ResponsiveController/ResponiveController.stories.ts
+++ packages/ui-lib/src/stories/ResponsiveController/ResponiveController.stories.ts
@@ -1,33 +1,37 @@
 import type { Meta, StoryObj } from '@storybook/svelte';
 import { ResponsiveController } from '../../lib/components/ResponsiveController/index.js';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-const meta = {
+// More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories
+const meta: Meta<typeof ResponsiveController> = {
 	title: 'Example/ResponsiveController',
 	component: ResponsiveController
 	//tags: ['autodocs'],
 	/*
-  argTypes: {
-    backgroundColor: { control: 'color' },
-    size: {
-      control: { type: 'select' },
-      options: ['small', 'medium', 'large'],
-    },
-  },*/
-} satisfies Meta<ResponsiveController>;
+	argTypes: {
+		backgroundColor: { control: 'color' },
+		size: {
+			control: { type: 'select' },
+			options: ['small', 'medium', 'large'],
+		},
+	},
+	*/
+} satisfies Meta<typeof ResponsiveController>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+// More on writing stories with args: https://storybook.js.org/docs/svelte/writing-stories/args
 export const Primary: Story = {
 	args: {
-		//	label: 'ResponsiveController'
+		label: 'ResponsiveController Primary',
+		// Add other props as needed
 	}
 };
 
 export const Secondary: Story = {
 	args: {
-		//	label: 'ResponsiveController'
+		label: 'ResponsiveController Secondary',
+		// Add other props as needed
 	}
 };
